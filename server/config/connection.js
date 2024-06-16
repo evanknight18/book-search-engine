4778f401e45bf060555a16d584b3f68d9c2fc63d
Evan <evanknight18@gmail.com>
2024-06-16 07:35:34 -0600

    connectionString connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Construct the MongoDB connection string from environment variables
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine';
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
