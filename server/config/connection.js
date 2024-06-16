029b6ed64ac3c5c88beb6f809b740bb78c7a7832
Evan <evanknight18@gmail.com>
2024-06-16 11:25:00 -0600

    simplify connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,8 @@
 const mongoose = require('mongoose');
 
-// Construct the MongoDB connection string from environment variables
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search-engine';
-
-mongoose.connect(connectionString, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose is connected to', connectionString);
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('Mongoose connection error:', err);
 });
 
 module.exports = mongoose.connection;
