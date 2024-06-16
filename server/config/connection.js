c2aaafa90c908630a2c2d6689bae3cd2c955f7dd
Evan <evanknight18@gmail.com>
2024-06-16 06:57:26 -0600

    mongo connection update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+// Construct the MongoDB connection string from environment variables
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks';
+
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected to', connectionString);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
 });
 
 module.exports = mongoose.connection;
